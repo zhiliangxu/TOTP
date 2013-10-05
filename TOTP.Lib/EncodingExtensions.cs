a1b87a3e1012996bd463654264f2185004f15325
Zhiliang Xu <xu.zl@live.com>
2013-10-05 15:54:42 +0800

    Add readonly constraint to the mapping variable

--- TOTP.Lib/EncodingExtensions.cs
+++ TOTP.Lib/EncodingExtensions.cs
@@ -3,7 +3,7 @@ namespace TOTP.Lib
 {
     public static class EncodingExtensions
     {
-        private static byte[] mapping = { 26, 27, 28, 29, 30, 255, 255, 255, 255, 255,
+        private static readonly byte[] mapping = { 26, 27, 28, 29, 30, 255, 255, 255, 255, 255,
                                         255, 255, 255, 255, 255, 0, 1, 2, 3, 4,
                                         5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
                                         15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
